a8b62787f9ab87d51b1c37a1e9fa554c322be097
KubaKijak <kijakkuba@gmail.com>
2022-02-22 15:00:59 +0100

    good

--- src/resources.py
+++ src/resources.py
@@ -9,30 +9,34 @@ class Character:
     def __str__(self) -> str:
         return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
     
-    def damage(self):
-        return self.attack
-    
     def take_damage(self, damage):
         relative_damage = damage - self.armor
         if relative_damage > 0:
             self.health -= relative_damage
         if self.health < 0: self.health = 0
+    
+    def get_attack(self): # tidigare damage
+        return self.attack
 
     def get_health(self):
         return self.health
+    
+    def get_name(self):
+        return self.name
+    
+    def get_attributes(self):
+        return self.name, self.health, self.attack, self.armor
 
 class Goblin:
     
-    def __init__(self, health, attack, armor):
+    def __init__(self, health, attack, armor, id):
         self.health = health
         self.attack = attack
         self.armor = armor
+        self.id = id
         
     def __str__(self) -> str:
-        return f"Goblin\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
-
-    def damage(self):
-        return self.attack
+        return f"Goblin #{self.id}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
     
     def take_damage(self, damage):
         relative_damage = damage - self.armor
@@ -42,3 +46,35 @@ class Goblin:
 
     def get_health(self):
         return self.health
+    
+    def get_attack(self):
+        return self.attack
+    
+    def get_name(self):
+        return f"Goblin #{self.id}"
+    
+
+def save_character(chars : list()):
+    save_list = []
+    for character in chars:
+        name, health, attack, armor = character.get_attributes()
+        save_string = f"{name}/{health}/{attack}/{armor}\n"
+        save_list.append(save_string)
+        
+    with open("saved_characters.txt", "w", encoding="utf8") as f:
+        for line in save_list:
+            f.write(line)
+        print("Characters has been saved!")
+
+def load_characters():
+    characters = []
+    with open("saved_characters.txt", "r", encoding="utf8") as f:
+        for line in f.readlines():
+            attributes = line.split("/")
+            char = Character(attributes[0],
+                             int(attributes[1]),
+                             int(attributes[2]),
+                             int(attributes[3]))
+            
+            characters.append(char)
+    return characters    
